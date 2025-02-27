e9229aa50754861815e3e7458f78410e3aedf789
Negoita Andrei <lilbabyfan04@gmail.com>
2025-02-27 16:39:56 +0200

    I created the project

---
+++ Calculator.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalculatorProject;
+
+namespace CalculatorProject;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+/// 
+public partial class MainWindow : Window
+{
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        ViewModel = new Calculator();
+        this.DataContext = ViewModel;
+    }
+
+    internal Calculator ViewModel { get; private set; }
+
+    private void DigitButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            ViewModel.AppendDigit(button.Content.ToString());
+        }
+    }
+}
---
+++ ViewModel/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject
+{
+    class Calculator:INotifyPropertyChanged
+    {
+        private string _displayText;
+
+        public string DisplayText
+        {
+            get { return _displayText; }
+            set
+            {
+                if (_displayText != value)
+                {
+                    _displayText = value;
+                    OnPropertyChanged(nameof(DisplayText));
+                }
+            }
+        }
+
+        public Calculator()
+        {
+            DisplayText = string.Empty; 
+        }
+
+        public void AppendDigit(string digit)
+        {
+            DisplayText += digit;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
